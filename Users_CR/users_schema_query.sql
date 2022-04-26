2d09545067fcca04bc688b9698f0822a5da057eb
Alexanderpmann <99506481+Alexanderpmann@users.noreply.github.com>
2022-04-25 22:23:49 -0400

    Users CRUD Modularized
    Modularized project from User_CR and User_CRUD

---
+++ Users_CR/users_schema_query.sql
@@ -0,0 +1,6 @@
+-- INSERT INTO users_schema.users (first_name, last_name, email, created_at)
+-- VALUES ("Alexander", "Mann", "test@testing.com", NOW());
+
+SELECT * FROM users_schema.users;
+-- DELETE FROM friends WHERE id IN (13,14);
+
